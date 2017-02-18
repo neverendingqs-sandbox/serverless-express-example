60f3bde14648745309759e1a9ab96122011bcd8e
Mark Tse <mark.tse@neverendingqs.com>
2017-02-18 15:03:47 -0500

    app - added sessions using DynamoDB.

---
+++ server-local.js
@@ -0,0 +1,15 @@
+const https = require('https');
+const selfSigned = require('openssl-self-signed-certificate');
+
+const app = require('./src/server');
+const port = (process.env.PORT || 3001);
+
+const options = {
+  key: selfSigned.key,
+  cert: selfSigned.cert
+};
+
+https.createServer(options, app).listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`HTTPS started on port ${port} (dev only).`);
