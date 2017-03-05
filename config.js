c96d1c4ef95e861196469269ca61aaf6dfb47f4e
Mark Tse <mark.tse@neverendingqs.com>
2017-03-05 14:10:39 -0500

    app - setting new env vars properly.

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports.env = () => {
   return {
-    SESSION_SECRET: process.env.SESSION_SECRET
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    PCO_CLIENT_ID: process.env.PCO_CLIENT_ID,
+    PCO_CLIENT_SECRET: process.env.PCO_CLIENT_SECRET
   };
 };
