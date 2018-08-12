74645acfdfe2adbde8c16bd423e703aa027ddc34
Mark Tse <mark.tse@neverendingqs.com>
2018-08-12 16:39:05 -0400

    app - updating ttl.

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -5,8 +5,10 @@ if(process.env.NODE_ENV === 'production' && typeof process.env.SESSION_SECRET ==
 const session = require('express-session');
 const DynamoDBStore = require('connect-dynamodb')({ session: session });
 
+const twoHoursInMs = 7200000;
+
 const sessionOptions = {
-  cookie: { maxAge: 7200000 },
+  cookie: { maxAge: twoHoursInMs },
   resave: false,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET || 'VERY INSECURE SECRET'
@@ -14,7 +16,7 @@ const sessionOptions = {
 
 if(process.env.SESSIONS_TABLE) {
   sessionOptions.store = new DynamoDBStore({
-    reapInterval: -1,
+    reapInterval: twoHoursInMs,
     table: process.env.SESSIONS_TABLE
   });
 }
