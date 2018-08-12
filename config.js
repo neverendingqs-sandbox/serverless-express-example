fbd7485e7e1d5cdb57b3138b297c3f9570b91e94
Mark Tse <mark.tse@neverendingqs.com>
2018-08-12 16:46:06 -0400

    chore - removing unused env vars.

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports.env = () => {
-  return {
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    PCO_CLIENT_ID: process.env.PCO_CLIENT_ID,
-    PCO_CLIENT_SECRET: process.env.PCO_CLIENT_SECRET
-  };
-};
